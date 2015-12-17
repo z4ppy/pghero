2205e01e207ffb55a71b0414e3f47ee9a5851ee6
Andrew Kane <acekane1@gmail.com>
2015-12-17 00:52:20 -0800

    Added connection_sources_by_database method

--- lib/pghero.rb
+++ lib/pghero.rb
@@ -332,6 +332,26 @@ module PgHero
         SELECT
           application_name AS source,
           client_addr AS ip,
+          COUNT(*) AS total_connections
+        FROM
+          pg_stat_activity
+        WHERE
+          pid <> pg_backend_pid()
+        GROUP BY
+          application_name,
+          ip
+        ORDER BY
+          COUNT(*) DESC,
+          application_name ASC,
+          client_addr ASC
+      SQL
+    end
+
+    def connection_sources_by_database
+      select_all <<-SQL
+        SELECT
+          application_name AS source,
+          client_addr AS ip,
           datname AS database,
           COUNT(*) AS total_connections
         FROM
